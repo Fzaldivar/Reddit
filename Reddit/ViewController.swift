b909101872c3356034a4aab384f36c49f9aa570e
Fernando Zaldívar Coto <repo_2203@hotmail.com>
2022-01-22 13:01:48 -0600

    Initial commit

---
+++ Reddit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reddit
+//
+//  Created by Fernando Zaldivar on 22/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
